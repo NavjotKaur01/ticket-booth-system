cff23b425bbbefb508cf59d2179d59224c346301
chandraprakash-ensue <chandra.prakash@ensuesoft.com>
2026-07-13 13:40:02 +0530

    temorary added the tunnel url and setup accordingly

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,9 @@
 
 interface ImportMetaEnv {
   readonly VITE_API_BASE_URL: string
+  // temporary-for-tunnel
+  readonly VITE_TUNNEL_URL: string
+  readonly VITE_USE_VENUE_TUNNEL: string
 }
 
 interface ImportMeta {
